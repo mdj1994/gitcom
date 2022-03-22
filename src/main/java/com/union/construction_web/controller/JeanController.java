a5ab37701032cb261b886f4be491e0bc46cddf1a
Mollang <hayaruby7909@gmail.com>
2022-03-22 16:31:51 +0900

    public - 공개제안

---
+++ src/main/java/com/union/construction_web/controller/JeanController.java
@@ -0,0 +1,45 @@
+package com.union.construction_web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JeanController extends BaseController {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Value("${API_SERVER.key}")
+    private String API_SERVER;
+
+    
+    
+    /**
+     * 공개제안 글 목록
+     * @return
+     */
+    @GetMapping("/jean/jean_list.do")
+    public String jean_list() {
+
+
+        return "jean/jean_list";
+    }
+
+
+    /**
+     * 공개제안 글쓰기
+     * @return
+     */
+    @GetMapping("/jean/jean_write.do")
+    public String jean_write() {
+
+        return "jean/jean_write";
+    }
+
+
+}
